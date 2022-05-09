620e228323ed831bede639bda0f22ad0067b3a4f
wangchen <wangchen130166@gmail.com>
2022-05-09 10:53:58 +0800

    add: 增加前端模版文件。

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -15,7 +15,7 @@ const App: React.FC = ({ children }) => {
   return (
     <>
       <Helmet>
-        <title>React-seed3-ts</title>
+        <title>react-typescript-dva-antd-template</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="" type="image/x-icon" />
       </Helmet>
